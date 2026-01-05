dbe82e9c36c20a985b65321d82a925385b78f2bd
pranjalphirke <90060614+pranjalphirke@users.noreply.github.com>
2026-01-05 21:49:56 +0530

    Create Vehicle class with attributes and methods

---
+++ Vehicle.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Vehicle {
+
+    private int id;
+    private String name;
+    private String type;
+    private boolean available;
+
+    public Vehicle(int id, String name, String type, boolean available) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+}
